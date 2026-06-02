0eec9ab38ee8c37a60b653d68d5ecc143f2d458d
Arjun Singh Rahal <arjun.rahal@amla.io>
2026-06-02 14:20:29 +0530

    Add Binary Tree problems

---
+++ Binary Tree/Binary Tree Traversal/InorderTraversal.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:   
+
+    // using recursion 
+        void helper(TreeNode* root , vector<int>& ans){
+        if(root== NULL) return ;
+        helper(root->left , ans);
+        ans.push_back(root->val);
+        helper(root->right , ans);
+        }
+    vector<int> inorderTraversal(TreeNode* root) {
+     vector<int> ans;
+    helper(root , ans);
+    return ans;
+    }
+    // using Stack data structure 
+    vector<int> inorderTraversal(TreeNode* root) {
+    stack<TreeNode*> st;
+    stack<bool> visited;
+    visited.push(0);
+    vector<int> finalans;
+    if(root == NULL) return finalans;
+    st.push(root);
+    while(!st.empty()){
+    TreeNode* temp = st.top();
+    st.pop();
+    bool flag =visited.top();
+    visited.pop();
+    if(!flag){
+     if(temp->right){st.push(temp->right);
+     visited.push(0);
+     };
+     st.push(temp);
+     visited.push(1);
+     if(temp->left){st.push(temp->left);
+     visited.push(0);
+     };
+    }
+    else{
+      finalans.push_back(temp->val) ;
+    }
+    }
+    return finalans;
+    }
+};
+ 
